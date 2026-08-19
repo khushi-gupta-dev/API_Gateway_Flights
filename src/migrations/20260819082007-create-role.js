a6bdf780388a4c9f190fcbda4d526ba9e6f54672
khushi-gupta-dev <khushi.gupta28.dev@gmail.com>
2026-08-19 21:00:11 +0530

    added models for many to many association between roles and users

---
+++ src/migrations/20260819082007-create-role.js
@@ -0,0 +1,35 @@
+const { enums } = require("../utils/common");
+const {ADMIN , CUSTOMER , FLIGHT_COMPANY } = enums.user_roles_enums;
+
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Roles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.ENUM,
+          values: [ADMIN, CUSTOMER, FLIGHT_COMPANY],
+        
+        allowNull: false,
+        defaultValue: CUSTOMER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Roles');
+  }
+};
---
+++ src/utils/common/enums.js
@@ -0,0 +1,9 @@
+user_roles_enums = {
+    ADMIN: "admin",
+    CUSTOMER: "customer",
+    FLIGHT_COMPANY: "flight_company"
+}
+
+module.exports = {
+user_roles_enums
+}
