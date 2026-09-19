9c432575e6f364114fa6ef26af0c4bf5e385d32b
khushi-gupta-dev <khushi.gupta28.dev@gmail.com>
2026-09-19 16:38:44 +0530

    Added authorisation for flight edit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ const rateLimit = require("express-rate-limit");
 
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-const {serverConfig , logger}  = require("./config")
+const {serverConfig , logger}  = require("./config");
 const apiRoutes = require("./routes");
-const app = express();
+const { authRequestMiddlewares } = require("./middlewares");
 
+const app = express();
 
 const limiter = rateLimit({
     windowMs: 2 * 60 * 1000, // 2 minutes
@@ -22,8 +23,21 @@ app.use(express.urlencoded({extended:true}));
 
 app.use(limiter);
 
-app.use('/flightsService' , createProxyMiddleware({target:serverConfig.FLIGHT_SERVICE , changeOrigin:true}))
-app.use('/bookingService' , createProxyMiddleware({target:serverConfig.BOOKING_SERVICE , changeOrigin:true}))
+app.post(
+    "/flightsService/api/v1/flights",
+    authRequestMiddlewares.checkAuth,
+    authRequestMiddlewares.isAdmin,
+);
+
+app.patch(
+    "/flightsService/api/v1/flights/:id/seats",
+    authRequestMiddlewares.checkAuth,
+    authRequestMiddlewares.isAdmin,
+);
+
+app.use('/flightsService' , createProxyMiddleware({target:serverConfig.FLIGHT_SERVICE , changeOrigin:true}));
+
+app.use('/bookingService' , createProxyMiddleware({target:serverConfig.BOOKING_SERVICE , changeOrigin:true}));
 
 app.use("/api", apiRoutes);
 
